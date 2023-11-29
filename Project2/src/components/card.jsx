bbc2492e195c01cbc526ce3f530e06a25bb8749f
kailaho <kaho3670@colorado.edu>
2023-11-28 17:16:45 -0700

    Card + Card.css

---
+++ Project2/src/components/card.jsx
@@ -0,0 +1,8 @@
+export default function PlanetCard(PlanetName, ImgPath){
+    return (
+        <div>
+            <h1>PlanetName</h1>
+            <img src="ImgPath"></img>
+        </div>
+    );
+}
