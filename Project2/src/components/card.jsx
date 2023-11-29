6dc9febb51dad811330a59c9e9c31f0923e77b1a
kailaho <kaho3670@colorado.edu>
2023-11-28 18:02:27 -0700

    new card layout

--- Project2/src/components/card.jsx
+++ Project2/src/components/card.jsx
@@ -1,12 +1,19 @@
 import './Card.css';
 
-export default function PlanetCard({PlanetName, ImgPath, PlanetDescription}){
+export default function PlanetCard({PlanetName, ImgPath, PlanetDescription, Distance, Diameter, Moons}){
     return (
         <div id="PlanetCard">
             <h1 id="CardTitle">{PlanetName}</h1>
             <div id="PlanetWrapper">
                 <img id="PlanetImage" src={ImgPath}></img>
+                <div id="DescHighlights">
                 <p id="PlanetDescription">{PlanetDescription}</p>
+                    <div id="Highlights">
+                        <p class="Highlight">Distance from Earth: {Distance}</p>
+                        <p class="Highlight">Diameter: {Diameter}</p>
+                        <p class="Highlight">Number of moons: {Moons}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
