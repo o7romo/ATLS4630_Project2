38636d18a28a2102f1853fce0ee7b7ae82a0d71f
o7romo <osro5694@colorado.edu>
2023-12-12 16:22:58 -0700

    amsodmao

---
+++ Project2/src/components/quiz.jsx
@@ -0,0 +1,12 @@
+const Quiz = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [answers, setAnswers] = useState([]);
+    const [showScore, setShowScore] = useState(false);
+    // Add quiz questions and answer options here
+    return (
+      <div>
+        {/* Render quiz UI */}
+      </div>
+    );
+  };
